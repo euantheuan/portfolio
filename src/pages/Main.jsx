45dba8eec20629b0652597886fd12c8fc40fef69
유안 <euantheuan@gmail.com>
2024-07-19 05:43:41 +0900

    intro modified

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,6 +9,9 @@ const Main = () => {
     return (
         <div>
             <div className="main_container">
+                <div className='moto'>
+                    "하고자 하면 방법이 보이고,<br />하지 않고자 하면 변명이 보인다"
+                </div>
                 <h1>웹 퍼블리셔 & 프론트엔드 개발자를 꿈꾸는</h1>
                 <h1><span>박유안</span>입니다 !</h1>
                 <div className="scroll">
@@ -19,20 +22,17 @@ const Main = () => {
 
             <div className="intro">
 
-                <div className='moto'>
-                    <span>"하고자 하면 방법이 보이고,<br />하지 않고자 하면 변명이 보인다"</span>
-                </div>
                 <div>
-                    <span>제 포트폴리오 페이지에 방문해주셔서 감사합니다.</span>
+                    제 포트폴리오 페이지에 방문해주셔서 감사합니다.
                 </div>
 
                 <div>
-                    <span>새롭고 낯선 상황/문화에 흥미를 느끼는<br /></span>
-                    박 유 안<span> 입니다.</span>
+                    새롭고 낯선 상황/문화에 흥미를 느끼는<br />
+                    <span>박유안</span> 입니다.
                 </div>
 
                 <div>
-                    <span>아래 링크를 통해 제 작업물 등을 열람하실 수 있습니다.</span>
+                    아래 링크를 통해 제 작업물 등을 열람하실 수 있습니다.
                 </div>
 
                 
